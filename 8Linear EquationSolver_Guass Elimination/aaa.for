e55f26793e70b409b1480413f2e1ce8571637f3a
Sunnieeee <1249053233@qq.com>
2019-10-09 01:40:07 +0800

    Fortran hw3 1009

--- 8Linear EquationSolver_Guass Elimination/aaa.for
+++ 8Linear EquationSolver_Guass Elimination/aaa.for
@@ -3,24 +3,27 @@ Program GE
 
     !**************变量声明****************!
     integer :: i
-    real*8 :: matb(1,9),matA(9,9)
+    real*8 :: matbb(9,1),matA(9,9),matb(1,9),matAb(9,10)
 
     !**************读取矩阵****************!
     open( file="b.txt", unit=10, action="read" )
-    read(10,*)matb
-    matb = transpose(matb)
+    read(10,*)matbb
+    matb = transpose(matbb)
 
-    open( file="b.txt", unit=10, action="read" )
+    open( file="A.txt", unit=10, action="read" )
     read(10,*)matA
-    matb = transpose(matA)
+    matA = transpose(matA)
 
     print "(a)", "vector b is:"
-    print "(9f8.0)",(matb(:,i),i=1,9)
+    print "(9f8.1)",(matb(:,i),i=1,9)
 
     print "(a)", "matrix A is:"
-    print "(9f8.0)",(matA(i,:),i=1,9)
+    print "(9f8.1)",(matA(i,:),i=1,9)
+
+    !**************增广矩阵****************!
+
 
     !****************三角化****************!
     
 
-end program GE
+end program GE 
